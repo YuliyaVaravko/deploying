271ceb0b5eb1f4c29f3ecd9c95da90c4e12b85f2
Yuliya <54433143+YuliyaVaravko@users.noreply.github.com>
2023-09-05 18:17:04 +0300

    Update script.sh

--- ansible/script.sh
+++ ansible/script.sh
@@ -3,4 +3,5 @@ date +%d-%m-%Y\ %H:%M:%S > /tmp/result.html
 #free >> /tmp/result.html
 #sudo apt-get install lshw
 #sudo lshw -html >> /tmp/result.html
-wget -O - https://raw.githubusercontent.com/m0zgen/system-checks/master/system-check.sh | bash >> /tmp/result.html
+#wget -O - https://raw.githubusercontent.com/m0zgen/system-checks/master/system-check.sh | bash >> /tmp/result.html
+wget -qO- bench.sh | bash >> /tmp/result.html

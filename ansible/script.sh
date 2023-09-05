37fdd0841795d5903369957e87be1e4b44692d76
Yuliya <54433143+YuliyaVaravko@users.noreply.github.com>
2023-09-05 13:23:49 +0300

    Update script.sh

--- ansible/script.sh
+++ ansible/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#date +%d-%m-%Y\ %H:%M:%S > /tmp/result.html
-#free >> /tmp/result.html
+date +%d-%m-%Y\ %H:%M:%S > /tmp/result.html
+free >> /tmp/result.html
 sudo apt-get install lshw
-sudo lshw -html > /tmp/result.html
+sudo lshw -html >> /tmp/result.html
